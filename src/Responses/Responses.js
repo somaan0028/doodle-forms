c79526fb17e52243dba7c6b912204d91445fe51c
somaan0028 <somaanshakeel0028@gmail.com>
2021-01-06 08:56:13 +0500

    Styling Responses page

--- src/Responses/Responses.js
+++ src/Responses/Responses.js
@@ -13,7 +13,8 @@ class Dashboard extends Component {
     formName: null,
     listOfResponses: null,
     generatedResponseList: null,
-    displayData: false
+    displayData: false,
+    formTime: '12-Jul-2077'
 
   }
 
@@ -44,7 +45,7 @@ class Dashboard extends Component {
     var listOfResponses = this.state.listOfResponses;
     var generatedResponseList = listOfResponses.map((response, index)=>{
       return <div key={index} >
-        <SingleResponse response={response} index={index}/>
+        <SingleResponse response={response} index={index} key={index}/>
       </div>
     })
     this.setState({
@@ -57,12 +58,16 @@ class Dashboard extends Component {
       return (
         <div className="responses">
         	<Navbar isAuth={true} />
-          <h2>Dashboard</h2>
-          <h3>Responses</h3>
-          <p>{this.state.formName}</p>
-  
-          {this.state.generatedResponseList}
-          <NavLink to="/create">Create New Form</NavLink>
+          <h2 className="responses-heading">Responses</h2>
+          <p className="form-name">{this.state.formName}</p>
+          <div className="form-details-container">
+            <p className="form-detail">{`No. of Responses: ${this.state.listOfResponses.length}`}</p>
+            <p className="form-detail">{`Created On: ${this.state.formTime}`}</p>
+          </div>
+          <div className="responses-list-container">
+            {this.state.generatedResponseList}
+          </div>
+          <NavLink className="back-to-dashboard" to="/dashboard">Back to Dashboard</NavLink>
         </div>
       );
     }else{
--- src/Responses/SingleResponse.js
+++ src/Responses/SingleResponse.js
@@ -6,12 +6,34 @@ const SingleResponse = ({response, index}) => {
     for (var question in response) {
         if (response.hasOwnProperty(question)) {
             console.log(question + " -> " + response[question]);
-            var toAppend = <p>{`${question}: ${response[question]}`}</p>
+            if (Array.isArray(response[question])) {
+                var checkboxAnswers = []
+                response[question].forEach(checkboxAnswer => {
+                    var answer = <p>{checkboxAnswer}</p>
+                    checkboxAnswers.push(answer);
+                });
+
+                // </div>
+                var toAppend = 
+                <div className="single-response-element">
+                    <h3 className="response-question">{question}</h3>
+                    <div>
+                        {checkboxAnswers}
+                    </div>
+                </div>
+            }else{
+                var toAppend = 
+                <div className="single-response-element">
+                    <h3 className="response-question">{question}</h3>
+                    <p>{response[question]}</p>
+                </div>
+            }
             generatedResponse.push(toAppend);
         }
     }
   return (
     <div className="single-response" key={index}>
+        <p className="response-number">{`Response No. ${index+1}`}</p>
         {generatedResponse}
     </div>
   );
